c0a54ab13475b12e5a2abf5150adbb7079fda48d
Senjai <r.crawfordwilson@gmail.com>
2013-10-14 23:37:14 +0000

    Updated Test

--- subscribem/spec/feature/accounts/sign_up_spec.rb
+++ subscribem/spec/feature/accounts/sign_up_spec.rb
@@ -5,6 +5,12 @@ feature 'Accounts' do
     visit subscribem.root_path
     click_link "Account Sign Up"
     fill_in 'Name', with: "Test"
+    fill_in 'Email', with: "subscribem@example.com"
+
+    password_field_id = "account_owner_attributes_password"
+    fill_in password_field_id, with: 'password'
+    fill_in "Password confirmation", with: 'password'
+
     click_button 'Create Account'
 
     success_message = 'Your account has been successfully created.'
