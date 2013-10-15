55c9259eb26a0c2f9ddb8043e30bc4f7fbd5dc32
Senjai <r.crawfordwilson@gmail.com>
2013-10-15 01:31:12 +0000

    Pre warden

--- subscribem/spec/dummy/db/schema.rb
+++ subscribem/spec/dummy/db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131015011318) do
+ActiveRecord::Schema.define(version: 20131015012410) do
 
   create_table "subscribem_accounts", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "owner_id"
+  end
+
+  create_table "subscribem_users", force: true do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
