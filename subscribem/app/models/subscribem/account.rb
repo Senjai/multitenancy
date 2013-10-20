ec01b85d034b68e3d1e2482e31c54daefc8cfbcc
Senjai <r.crawfordwilson@gmail.com>
2013-10-20 20:52:18 +0000

    Dont allow subdomains with restricted names like admin

--- subscribem/app/models/subscribem/account.rb
+++ subscribem/app/models/subscribem/account.rb
@@ -1,8 +1,15 @@
 module Subscribem
   class Account < ActiveRecord::Base
+    before_validation do
+      self.subdomain = subdomain.to_s.downcase
+    end
+    EXCLUDED_SUBDOMAINS = %w(admin)
+
     belongs_to :owner, :class_name => "Subscribem::User"
     accepts_nested_attributes_for :owner
 
     validates :subdomain, uniqueness: true, presence: true
+    validates_exclusion_of :subdomain, in: EXCLUDED_SUBDOMAINS,
+                                       message: "is not allowed. Please choose another subdomain."
   end
 end
