6e43c985d58a04991a4a38b6b209bcb499aaddef
Senjai <r.crawfordwilson@gmail.com>
2013-10-20 00:30:38 +0000

    Added initalizer for warden

--- subscribem/lib/subscribem/engine.rb
+++ subscribem/lib/subscribem/engine.rb
@@ -1,9 +1,15 @@
+require 'warden'
+
 module Subscribem
   class Engine < ::Rails::Engine
     isolate_namespace Subscribem
 
     config.generators do |g|
       g.test_framework :rspec, :view_specs => false
+    end
+
+    initializer "subscribem.middleware.warden" do
+      Rails.application.config.middleware.use Warden::Manager
     end
   end
 end
