6563fa5ad614cce1242d9b570b21dcb642850828
Senjai <r.crawfordwilson@gmail.com>
2013-10-22 06:47:58 +0000

    rails g rspec:install

--- hosted_forums/config/routes.rb
+++ hosted_forums/config/routes.rb
@@ -2,7 +2,7 @@ require 'subscribem/constraints/subdomain_required'
 
 HostedForums::Application.routes.draw do
 
-  constraints(Subscribem::Constraints::Subdomain) do
+  constraints(Subscribem::Constraints::SubdomainRequired) do
     mount Forem::Engine, at: "/"
   end
 
