ea9dff4f2fb31aca2564ad30e88ce4180649ba3c
Senjai <r.crawfordwilson@gmail.com>
2013-10-22 06:42:32 +0000

    Installed forem, modified forem route.

--- hosted_forums/config/routes.rb
+++ hosted_forums/config/routes.rb
@@ -1,3 +1,10 @@
+require 'subscribem/constraints/subdomain_required'
+
 HostedForums::Application.routes.draw do
+
+  constraints(Subscribem::Constraints::Subdomain) do
+    mount Forem::Engine, at: "/"
+  end
+
   mount Subscribem::Engine, at: "/"
 end
