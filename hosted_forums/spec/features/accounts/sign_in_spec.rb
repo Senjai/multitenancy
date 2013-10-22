1bd75d6f2beb5aaa6e2f2b3423341fc9b32e1a81
Senjai <r.crawfordwilson@gmail.com>
2013-10-22 07:18:34 +0000

    WIP

---
+++ hosted_forums/spec/features/accounts/sign_in_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+require 'subscribem/testing_support/subdomain_helpers'
+
+feature "user sign in" do
+  extend Subscribem::TestingSupport::SubdomainHelpers
+
+  let!(:account) { FactoryGirl.create(:account_with_schema) }
+  let(:sign_in_url) { "http://#{account.subdomain}.example.com/sign_in" }
+  let(:root_url) { "http://#{account.subdomain}.example.com/" }
+
+  within_account_subdomain do
+    scenario "signs in as account owner successfully" do
+      visit root_url
+      page.current_url.should == sign_in_url
+      fill_in "Email", with: account.owner.email
+      fill_in "Password", with: "password"
+      click_button "Sign In"
+      page.should have_content("You are now signed in.")
+      page.current_url.should == root_url
+    end
+  end
+end
