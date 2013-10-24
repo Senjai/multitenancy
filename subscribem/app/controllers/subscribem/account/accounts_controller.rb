6b2b1f27078ff467487f735940b944b88fdcac3f
Senjai <r.crawfordwilson@gmail.com>
2013-10-24 05:19:50 +0000

    Fixed! Warden seemed to only login users for one request.

--- subscribem/app/controllers/subscribem/account/accounts_controller.rb
+++ subscribem/app/controllers/subscribem/account/accounts_controller.rb
@@ -3,7 +3,6 @@ require_dependency "subscribem/application_controller"
 module Subscribem
   class Account::AccountsController < ApplicationController
     def update
-      binding.pry
       if current_account.owner?(current_user) && current_account.update_attributes(account_params)
         flash[:success] = "Account updated successfully."
         redirect_to root_url
--- subscribem/spec/feature/accounts/updating_spec.rb
+++ subscribem/spec/feature/accounts/updating_spec.rb
@@ -8,15 +8,15 @@ feature "Accounts" do
   let!(:account) {FactoryGirl.create(:account_with_schema)}
   let(:root_url) {"http://#{account.subdomain}.example.com"}
 
-  before do
-    sign_in_as(user: account.owner, account: account)
-  end
-
   context "as the account owner" do
     scenario "Updating an account" do
       visit root_url
+      fill_in "Email", :with => account.owner.email
+      fill_in "Password", :with => "password"
+      click_button "Sign In"
       click_link "Edit Account"
       fill_in "Name", with: "A new name"
+      sign_in_as(user:account.owner, account: account)
       click_button "Update Account"
       page.should have_content("Account updated successfully.")
       account.reload.name.should == "A new name"
